2660603717d98188e9344bb91af03d13228ba759
Emanuel Meier Martins <emanuelmmartins@gmail.com>
2025-02-28 19:22:22 -0300

    Adicionei método sayHelloInSpanish() para exibir 'Hola que tal'

--- Aula02/Hello.java
+++ Aula02/Hello.java
@@ -2,9 +2,14 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
         sayHelloInPortuguese();
+        sayHelloInSpanish();
     }
 
      public static void sayHelloInPortuguese() {
         System.out.println("Olá Mundo");
+    }
+
+     public static void sayHelloInSpanish() {
+        System.out.println("Hola que tal");
     }
 }
